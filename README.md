c658a20d28f3c97c4b32d136c51f3bea41ef9bdc
Sahil Sharma <sahil.sharma1912@gmail.com>
2022-04-19 22:25:46 +0530

    UPDATE :: Max Consecutive Ones

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 If you have better solutions, you can share with others too! Thank you in advance 😊
 
 [Last Stone Weight](/src/LastStoneWeight.java)\
+[Max Consecutive Ones](/src/MaxConsecutiveOnes.java)\
 [Palindrome](/src/Palindrome.java)\
 [Remove Duplicate](/src/RemoveDuplicate.java)\
 [Two Sum](/src/TwoSum.java)
---
+++ src/MaxConsecutiveOnes.java
@@ -0,0 +1,49 @@
+public class MaxConsecutiveOnes {
+
+    public static void main(String[] args) {
+        int[] nums = {1,0,1,1,0,1};
+
+        System.out.println(findMaxConsecutiveOnes(nums));
+    }
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        short length = 0;
+        short result = 0;
+        for (int elem : nums) {
+            if (elem == 0) {
+                length = 0;
+            } else {
+                length++;
+                result = (short)Math.max(length, result);
+            }
+        }
+        return result;
+    }
+}
+
+/**
+ * Given a binary array nums, return the maximum number of consecutive 1's in the array.
+ *
+ * ----
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,1,0,1,1,1]
+ *
+ * Output: 3
+ *
+ * Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+ *
+ * ----
+ *
+ * Example 2:
+ *
+ * Input: nums = [1,0,1,1,0,1]
+ * Output: 2
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 105
+ * nums[i] is either 0 or 1.
+ */
