9dae6a28c76832f17a7dcbfe9afa9d6a1628e912
Sahil Sharma <sahil.sharma1912@gmail.com>
2023-08-13 13:55:56 +0530

    UPDATE :: star patter 8

--- src/starPatterns/StarPatterns.java
+++ src/starPatterns/StarPatterns.java
@@ -104,5 +104,23 @@ public class StarPatterns {
             System.out.println();
         }
 
+        System.out.println();
+        System.out.println();
+
+        /** PATTERN - 8 */
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = rows - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 }
