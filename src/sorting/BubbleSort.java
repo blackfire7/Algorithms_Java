7cadd3e39cc986faa8f382ee68c10e5cffecf2df
Sahil Sharma <sahil.sharma1912@gmail.com>
2023-08-16 23:45:55 +0530

    bubble sort

---
+++ src/sorting/BubbleSort.java
@@ -0,0 +1,27 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] array = {4, 2, 8, 1, 3, 7};
+
+        System.out.println(Arrays.toString(bubbleSort(array)));
+    }
+
+    public static int[] bubbleSort(int[] array) {
+        int length = array.length;
+
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = 0; j < length - i - 1; j++) {
+                if (array[j] > array[j+1]) {
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+
+        return array;
+    }
+}
