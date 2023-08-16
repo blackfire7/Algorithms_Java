fc4097eef69f63a4da0107a032561cd4006fe6bc
Sahil Sharma <sahil.sharma1912@gmail.com>
2023-08-16 23:50:00 +0530

    bubble sort

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args) {
         int[] array = {4, 2, 8, 1, 3, 7};
-
         System.out.println(Arrays.toString(bubbleSort(array)));
     }
 
