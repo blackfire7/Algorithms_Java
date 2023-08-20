0557d14320c8116be245f69182ddfa2d7b511278
Sahil Sharma <sahil.sharma1912@gmail.com>
2023-08-21 00:38:50 +0530

    update :: subsets of string

--- src/recursion/SubsetOfString.java
+++ src/recursion/SubsetOfString.java
@@ -6,30 +6,28 @@ import java.util.List;
 
 public class SubsetOfString {
 
-    public static List<String> subsets = new ArrayList<String>();
-
     public static void main(String[] args) {
-        System.out.println(findSubstrings("abc"));
+        String str = "abc";
+        System.out.println(findSubstrings(str));
     }
 
-    public static List<String> findSubstrings(String s) {
-        return printAllSubsets(s, 0, "");
-    }
+    public static List<String> findSubstrings(String str) {
+        ArrayList<String> subsets = new ArrayList<>();
 
-
-    public static List<String> printAllSubsets(String s, int i, String cur) {
-
-        if (i == s.length()) {
-            if (!cur.equals("")) subsets.add(cur);
-            System.out.println(cur);
-            return subsets;
-        }
-
-        printAllSubsets(s, i + 1, cur + s.charAt(i));
-        printAllSubsets(s, i + 1, cur);
-
+        helper(str, subsets, 0, "");
         Collections.sort(subsets);
+        subsets.remove(0);
 
         return subsets;
+    }
+
+    public static void helper(String s, List<String> ans, int i, String cur) {
+        if (i == s.length()) {
+            ans.add(cur);
+            return;
+        }
+
+        helper(s, ans, i + 1, cur + s.charAt(i));
+        helper(s, ans, i + 1, cur);
     }
 }
