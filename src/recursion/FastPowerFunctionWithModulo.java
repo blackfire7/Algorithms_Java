212541f71b00a062ef04deb960060d014ddd394b
Sahil Sharma <sahil.sharma1912@gmail.com>
2023-08-20 22:42:34 +0530

    recursion :: updated fast power with modulo

--- src/recursion/FastPowerFunctionWithModulo.java
+++ src/recursion/FastPowerFunctionWithModulo.java
@@ -3,10 +3,13 @@ package recursion;
 public class FastPowerFunctionWithModulo {
 
     public static void main(String[] args) {
-        System.out.println(myPow(2, 5, 2));
+        System.out.println(myPow(7, 7, 30));
     }
 
-    public static double myPow(double x, int n, int p) {
-        return (Math.pow(x, n)) % p;
+    public static double myPow(double a, int b, int m) {
+        if (b == 0) return 1;
+        else if (b < 0) return myPow(1/a, Math.abs(b), m);
+        else if (b % 2 == 0) return myPow((a % m * a % m) % m, b/2, m);
+        else return (a % m * myPow(a, b - 1, m) % m) % m;
     }
 }
