781d1dedb96ee1ae330b8a1505468125d8cd2a7b
Sahil Sharma <sahil.sharma1912@gmail.com>
2023-08-21 00:40:53 +0530

    changes for fast power

--- src/recursion/FastPowerFunctionWithModulo.java
+++ src/recursion/FastPowerFunctionWithModulo.java
@@ -6,10 +6,10 @@ public class FastPowerFunctionWithModulo {
         System.out.println(myPow(7, 7, 30));
     }
 
-    public static double myPow(double a, int b, int m) {
-        if (b == 0) return 1;
-        else if (b < 0) return myPow(1/a, Math.abs(b), m);
-        else if (b % 2 == 0) return myPow((a % m * a % m) % m, b/2, m);
-        else return (a % m * myPow(a, b - 1, m) % m) % m;
+    public static double myPow(double x, int n, int p) {
+        if (n == 0) return 1;
+        else if (n < 0) return myPow(1/x, Math.abs(n), p);
+        else if (n % 2 == 0) return myPow((x % p * x % p) % p, n/2, p);
+        else return (x % p * myPow(x, n - 1, p) % p) % p;
     }
 }
