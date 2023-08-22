c16d83d0c8678d6fbcc57d889aefa342aa84579c
Sahil Sharma <sahil.sharma1912@gmail.com>
2023-08-22 22:25:54 +0530

    backtracking :: nights tour fixes

--- src/backtracking/KnightsTourProblem.java
+++ src/backtracking/KnightsTourProblem.java
@@ -3,8 +3,10 @@ package backtracking;
 public class KnightsTourProblem {
 
     public static void main(String[] args) {
-        int n = 5;
-        int m = 5;
+
+        /* 1 <= N, M <= 7 */
+        int n = 5, m = 5;
+
         int[][] a = nightsTour(n, m);
 
         for (int i = 0; i < n; i++) {
@@ -17,13 +19,17 @@ public class KnightsTourProblem {
 
     public static int[][] nightsTour(int n, int m) {
         int[][] a = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
                 a[i][j] = -1;
-            }
+
+        if (n * 2 == m || m * 2 == n) return a;
+        if (n > 2 && m > 2) a[0][0] = 0;
+        if (n == 1 && m == 1) {
+            a[0][0] = 0;
+            return a;
         }
 
-        a[0][0] = 0;
         int[] movesX = {2, 1, -1, -2, -2, -1, 1, 2};
         int[] movesY = {1, 2, 2, 1, -1, -2, -2, -1};
         nightsTourHelper(n, m, a, movesX, movesY, 0, 0, 1);
@@ -31,13 +37,13 @@ public class KnightsTourProblem {
     }
 
     public static boolean nightsTourHelper(int n, int m, int[][] a, int[] movesX, int[] movesY, int curX, int curY, int step) {
-        if (step == n * n) return true;
+        if (step == n * m) return true;
 
         for (int i = 0; i < 8; i++) {
             int nextX = curX + movesX[i];
             int nextY = curY + movesY[i];
 
-            if (isValid(n, m, a, nextX, nextY)) {
+            if (nextX >= 0 && nextX < n && nextY >= 0 && nextY < m && a[nextX][nextY] == -1) {
                 a[nextX][nextY] = step;
                 if (nightsTourHelper(n, m, a, movesX, movesY, nextX, nextY, step + 1)) return true;
                 else a[nextX][nextY] = -1;
@@ -45,10 +51,6 @@ public class KnightsTourProblem {
         }
 
         return false;
-    }
-
-    public static boolean isValid(int n, int m, int[][] a, int x, int y) {
-        return x >= 0 && y >= 0 && x < n && y < m && a[x][y] == -1;
     }
 
 }
