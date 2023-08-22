ce8be2b5705ed96d784ac307bd1efa422917e132
Sahil Sharma <sahil.sharma1912@gmail.com>
2023-08-22 22:30:21 +0530

    backtracking permutations of an array

---
+++ src/backtracking/PermutationsOfAnArray.java
@@ -0,0 +1,13 @@
+package backtracking;
+
+public class PermutationsOfAnArray {
+    public static void main(String[] args) {
+        /*
+        * 1 <= nums.length <= 6
+        * -10 <= nums[i] <= 10
+        * All the integers of nums are unique.
+        */
+
+
+    }
+}
