c1e9e8abb6098f2a31657274f0b298665f6391d0
Sahil Sharma <sahil.sharma1912@gmail.com>
2023-08-27 20:31:09 +0530

    backtracking :: permutation of string

---
+++ src/backtracking/PermutationOfString.java
@@ -0,0 +1,45 @@
+package backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PermutationOfString {
+
+    public static void main(String[] args) {
+//        String str = "ABC";
+        String str = "AAA";
+        List<String> result = permute(str);
+        result = result.stream().sorted().collect(Collectors.toList());
+        System.out.println(result);
+    }
+
+
+    public static List<String> permute(String str) {
+        int n = str.length();
+        List<String> strList = new ArrayList<>();
+        permuteHelper(str, strList, 0, n - 1);
+        return strList;
+    }
+
+    public static void permuteHelper(String str, List<String> strList, int l, int r) {
+        if (l == r)
+            strList.add(str);
+        else {
+            for (int i = l; i <= r; i++) {
+                str = swap(str, l, i);
+                permuteHelper(str, strList, l + 1, r);
+                str = swap(str, l, i);
+            }
+        }
+    }
+
+    public static String swap(String a, int i, int j) {
+        char temp;
+        char[] charArray = a.toCharArray();
+        temp = charArray[i];
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+        return String.valueOf(charArray);
+    }
+}
