e2ca81b94a1ff8ab5ffc76d33c1ac77e73fc0884
Sahil Sharma <sahil.sharma1912@gmail.com>
2023-08-27 19:53:30 +0530

    UPDATE :: permutations of an array

--- src/backtracking/PermutationsOfAnArray.java
+++ src/backtracking/PermutationsOfAnArray.java
@@ -1,11 +1,35 @@
 package backtracking;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class PermutationsOfAnArray {
+
     public static void main(String[] args) {
-        /*
-        * 1 <= nums.length <= 6
-        * -10 <= nums[i] <= 10
-        * All the integers of nums are unique.
-        */
+//        int[] nums = {-1, -2, -3, -4, -5};
+        int[] nums = {1, 2, 3};
+        System.out.println(permute(nums));
+    }
+
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums);
+        return list;
+    }
+
+    public static void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
+        if(tempList.size() == nums.length){
+            list.add(new ArrayList<>(tempList));
+        } else{
+            for (int num : nums) {
+                if (tempList.contains(num)) continue;
+                tempList.add(num);
+                backtrack(list, tempList, nums);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+
     }
 }
