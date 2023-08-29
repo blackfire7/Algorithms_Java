e2c13aa2a27a0fc22ab90de47c40c1cb973389b4
Sahil Sharma <sahil.sharma1912@gmail.com>
2023-08-29 21:59:53 +0530

    subsequences of given array

--- src/backtracking/SubsequenceOfGivenArray.java
+++ src/backtracking/SubsequenceOfGivenArray.java
@@ -1,8 +1,6 @@
 package backtracking;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.*;
 
 public class SubsequenceOfGivenArray {
     public static void main(String[] args) {
@@ -14,21 +12,21 @@ public class SubsequenceOfGivenArray {
     }
 
     public static ArrayList<ArrayList<Integer>> AllSubsets(ArrayList<Integer> nums) {
+        Collections.sort(nums);
         ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
-        nums.sort(Comparator.naturalOrder());
-        helperSequence(nums, ans, 0, new ArrayList<>());
+        helperSequence(nums, 0, new ArrayList<>(), ans);
         return ans;
     }
 
-    public static void helperSequence(ArrayList<Integer> nums, ArrayList<ArrayList<Integer>> ans, int index, ArrayList<Integer> cur) {
-        ArrayList<Integer> curCopy = new ArrayList<>(cur);
-        if (!ans.contains(cur))
-            ans.add(curCopy);
+    public static void helperSequence(ArrayList<Integer> nums, int index, ArrayList<Integer> cur, ArrayList<ArrayList<Integer>> Ans) {
+        ArrayList<Integer> copyNums = new ArrayList<>(cur);
+        Ans.add(copyNums);
 
         for (int i = index; i < nums.size(); i++) {
+            if (i > index && nums.get(i)==nums.get(i-1)) continue;
             cur.add(nums.get(i));
-            helperSequence(nums, ans, i + 1, cur);
-            cur.remove(cur.size() - 1);
+            helperSequence(nums, i+1, cur, Ans);
+            cur.remove(cur.size()-1);
         }
     }
 }
