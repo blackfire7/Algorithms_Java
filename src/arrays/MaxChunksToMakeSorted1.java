25dad3db7e088ab5659b0463d62f2977c3beac13
Sahil Sharma <sahil.sharma1912@gmail.com>
2023-09-17 22:36:39 +0530

    arrays :: max chunks 1

---
+++ src/arrays/MaxChunksToMakeSorted1.java
@@ -0,0 +1,28 @@
+package arrays;
+
+public class MaxChunksToMakeSorted1 {
+    public static void main(String[] args) {
+        int[] a = {2, 0, 1, 5, 3, 4, 6};
+        System.out.println(maxChunks(a));
+    }
+
+    public static int maxChunks(int[] a) {
+        if (a.length == 0) return 0;
+
+        int chunks = 0;
+        int maxTillNow = a[0];
+
+        for (int i = 0; i < a.length; i++) {
+            maxTillNow = Math.max(maxTillNow, a[i]);
+            if (i == maxTillNow) chunks++;
+        }
+
+        return chunks;
+    }
+
+    /**
+     * You are given an integer array A of length n that represents a permutation of the integers in the range [0, n - 1].
+     * We split A into some number of Partitions, and individually sort each chunk. After joining them, the result should equal the sorted array.
+     * Return the largest number of chunks we can make to sort the array.
+     */
+}
