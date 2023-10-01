e908cd0c71868b6441dab35551909708b0b78a43
Sahil Sharma <sahil.sharma1912@gmail.com>
2023-10-01 12:12:24 +0530

    linked list :: remove duplicates comment added

--- src/linkedList/RemoveDuplicate.java
+++ src/linkedList/RemoveDuplicate.java
@@ -49,4 +49,31 @@ public class RemoveDuplicate {
         return newHead;
     }
 
+    /**
+     * Problem Statement
+     Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+     Example 1:
+     Input:
+
+     head = [-1, -1, 2, 2, 3, 3, 3]
+
+     Output:
+
+     [-1, 2, 3]
+
+     Example 2:
+     Input:
+
+     head = [-1, 1, 2, 2]
+
+     Output:
+
+     [-1, 1, 2]
+
+     Constraints:
+     -300 <= node value <= 300
+
+     Each list has atmost 100 nodes*/
+
 }
