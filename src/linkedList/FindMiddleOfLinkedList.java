d1faaaf63405b36f867488c24a1378725acbd952
Sahil Sharma <sahil.sharma1912@gmail.com>
2023-10-03 22:19:50 +0530

    middle node implementation

--- src/linkedList/FindMiddleOfLinkedList.java
+++ src/linkedList/FindMiddleOfLinkedList.java
@@ -1,4 +1,8 @@
 package linkedList;
 
 public class FindMiddleOfLinkedList {
+    public static int getMiddle(ListNode head) {
+        int count = DeleteMiddleNode.countNodes(head);
+        return count / 2;
+    }
 }
