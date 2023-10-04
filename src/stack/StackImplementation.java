a196dd60cd40b54333696b54556db631adb785ad
Sahil Sharma <sahil.sharma1912@gmail.com>
2023-10-04 23:19:33 +0530

    stack implementation

---
+++ src/stack/StackImplementation.java
@@ -0,0 +1,15 @@
+package stack;
+
+public class StackImplementation {
+    public static void main(String[] args) {
+        StackUsingArray stack = new StackUsingArray(4);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+
+        stack.printStack();
+        System.out.println(stack.pop());
+        stack.printStack();
+    }
+}
--- src/stack/StackUsingArray.java
+++ src/stack/StackUsingArray.java
@@ -1,2 +1,56 @@
-package stack;public class StackUsingArray {
+package stack;
+
+public class StackUsingArray {
+    private int size;
+    private int[] a;
+    private int top;
+
+    StackUsingArray(int size) {
+        this.size = size;
+        a = new int[size];
+        top = -1;
+    }
+
+    public boolean isEmpty() {
+        return this.top == -1;
+    }
+
+    public boolean isFull() {
+        return this.top == size - 1;
+    }
+
+    public boolean push(int data) {
+        if (isFull()) {
+            System.out.println("Stack is full!");
+            return false;
+        }
+        top++;
+        a[top] = data;
+        return true;
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty!");
+            return -1;
+        }
+
+        return a[top--];
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty!");
+            return -1;
+        }
+
+        return a[top];
+    }
+
+    public void printStack() {
+        for (int i = 0; i <= top; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
 }
