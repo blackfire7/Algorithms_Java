369f2ff629a6f243880d75a41fbe85a536301477
Sahil Sharma <sahil.sharma1912@gmail.com>
2023-10-06 22:44:01 +0530

    Parenthesis matching implementation

--- src/stack/ParenthesisMatching.java
+++ src/stack/ParenthesisMatching.java
@@ -1,7 +1,30 @@
 package stack;
 
+import java.util.ArrayDeque;
+
 public class ParenthesisMatching {
     public static void main(String[] args) {
+        System.out.println(validParenthesis("(){}[]"));
+    }
 
+    public static boolean validParenthesis(String s) {
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+
+            if (cur == '(' || cur == '{' || cur == '[') {
+                stack.push(cur);
+            } else {
+                if (stack.isEmpty()) return true;
+
+                char opening = stack.pop();
+
+                if ((opening == '(' && cur == ')') || (opening == '{' && cur == '}') || (opening == '[' && cur == ']')) {
+                } else return false;
+            }
+        }
+
+        return stack.isEmpty();
     }
 }
